5ba255d22ae626cf21a26ef3212642e7519b93d0
ivapie <ivanp@arcion.io>
2022-07-12 17:02:43 -0600

    Hello

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload vanchis.
+          Edit <code>src/App.js</code> and save to reload Ivanchis.
         </p>
         <a
           className="App-link"
