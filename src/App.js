986b1ca9b88bc6066800c118344793e4566c0dfd
ivapie <ivanp@arcion.io>
2022-07-12 17:51:09 -0600

    Change chromatic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload ivanchis.
+          Edit <code>src/App.js</code> and save to reload IIvanchis.
         </p>
         <a
           className="App-link"
